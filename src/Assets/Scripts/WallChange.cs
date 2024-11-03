aad0c39d86e8a5a83401452db6589f236dde6d49
Andrew Maney <xxxmemescoepxxx@gmail.com>
2024-11-03 08:14:56 -0500

    Update wall change script

--- src/Assets/Scripts/WallChange.cs
+++ src/Assets/Scripts/WallChange.cs
@@ -5,6 +5,8 @@ public class WallChange : MonoBehaviour
     /* VARIABLES */
     public Material BreakOutMaterialAccent;
     public Material BreakOutMaterialWall;
+    public Material DefaultMaterialAccent;
+    public Material DefaultMaterialWall;
     public GameObject FinalBoss;
 
 
@@ -12,7 +14,29 @@ public class WallChange : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (GameObject Wall in GameObject.FindGameObjectsWithTag("Wall"))
+        {
+            var MeshRendererComponent = Wall.GetComponent<MeshRenderer>();
+
+            if (MeshRendererComponent == null)
+            {
+                continue;
+            }
+
+            MeshRendererComponent.material = DefaultMaterialWall;
+        }
+
+        foreach (GameObject Accent in GameObject.FindGameObjectsWithTag("Accents"))
+        {
+            var MeshRendererComponent = Accent.GetComponent<MeshRenderer>();
+
+            if (MeshRendererComponent == null)
+            {
+                continue;
+            }
+
+            MeshRendererComponent.material = DefaultMaterialAccent;
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +53,7 @@ public class WallChange : MonoBehaviour
                     continue;
                 }
 
-                MeshRendererComponent.materials[0] = BreakOutMaterialWall;
+                MeshRendererComponent.material = BreakOutMaterialWall;
             }
 
             foreach (GameObject Accent in GameObject.FindGameObjectsWithTag("Accents"))
@@ -41,7 +65,7 @@ public class WallChange : MonoBehaviour
                     continue;
                 }
 
-                MeshRendererComponent.materials[0] = BreakOutMaterialAccent;
+                MeshRendererComponent.material = BreakOutMaterialAccent;
             }
 
             Destroy(this.gameObject);
